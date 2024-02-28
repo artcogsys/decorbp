7e569fadbd3088ebc2f06c14eaca8e6f4b828ac2
Marcel van Gerven <marcel.van.gerven@gmail.com>
2024-02-28 08:59:05 +0100

    update

--- decorrelation/decorrelation.py
+++ decorrelation/decorrelation.py
@@ -33,9 +33,11 @@ def lower_triangular(C: Tensor, offset: int):
 class Decorrelation(nn.Module):
     """A Decorrelation layer flattens the input, decorrelates, updates decorrelation parameters, and returns the reshaped decorrelated input"""
 
-    def __init__(self, in_features: int, bias: bool = False, device=None, dtype=None) -> None:
+    def __init__(self, in_features: int, bias: bool = False, eta: float = 1e-5, device=None, dtype=None) -> None:
         """"Params:
-            - dim: input dimensionality
+            - in_features: input dimensionality
+            - bias: whether or not to demean the data
+            - eta: learning rate
         """
 
         factory_kwargs = {'device': device, 'dtype': dtype}
@@ -44,6 +46,7 @@ class Decorrelation(nn.Module):
         self.in_features = in_features
         self.weight = nn.Parameter(torch.empty(self.in_features, self.in_features, **factory_kwargs), requires_grad=False)
         self.bias = nn.Parameter(torch.empty(in_features, **factory_kwargs), requires_grad=False) if bias else None
+        self.eta = eta
         self.X = None
 
         self.reset_parameters()
@@ -62,7 +65,7 @@ class Decorrelation(nn.Module):
         """
         return F.linear(input.view(len(input), -1), self.weight)
 
-    def update(self, eta):
+    def update(self):
         """Implements whitened Gram-Schmidt decorrelation update"""
 
         # # strictly lower triangular part of x x' averaged over datapoints
@@ -77,24 +80,29 @@ class Decorrelation(nn.Module):
         # # compute loss; equation (1) in technical note
         # return (1.0/self.in_features) * ( 2*(1-self.kappa)/(self.in_features - 1) * torch.trace(L @ L.T) + self.kappa * torch.trace(V @ V.T) )
 
+        # update *= normalizer[:, None]
+
+        # # Note that if we wish to use this with an SGD optimiser we have to be a little careful:
+        # update += (1 - normalizer)[:, None] * self.weight
+        
+        # self.weight.data = update @ self.weight.data
+
+        # Update grads of decorrelation matrix
+        # self.weight.grad = grads
+
+        # self.weight -= eta * self.weight.grad
+
         # If using a bias, it should demean the data
         if self.bias is not None:
             self.bias.grad = self.decor_state.sum(axis=0)
 
         # The full correlation matrix = (1/batch_size)*(x.T @ x)
         corr = (1/len(self.decor_state))*(self.decor_state.transpose(0, 1) @ self.decor_state)
-        grads = corr @ self.weight.data
 
         normalizer = 1.0 / (torch.mean(self.decor_state**2, axis=0))
         normalizer[torch.mean(self.decor_state**2, axis=0) < 1e-8] = 1.0
 
-        grads *= normalizer[:, None]
-
-        # Note that if we wish to use this with an SGD optimiser we have to be a little careful:
-        grads += (1 - normalizer)[:, None] * self.weight
-        
-        # Update grads of decorrelation matrix
-        self.weight.grad = grads
+        self.weight.data = normalizer[:, None] * (torch.eye(corr.shape[0]) - self.eta * corr @ self.weight) @ self.weight.data
 
         return 0.5 * torch.mean(torch.square(corr - torch.eye(corr.shape[0])))
 
@@ -115,13 +123,14 @@ class DecorConv2d(Decorrelation):
     """2d convolution with input decorrelation"""
 
     def __init__(self, in_channels: int, out_channels: int, kernel_size: _size_2_t,
-                 stride: _size_2_t = 1, padding: _size_2_t = 0, dilation: _size_2_t = 1, bias: bool = False,
-                 downsample_perc=1.0, kappa=0.0, device=None, dtype=None) -> None:
+                 stride: _size_2_t = 1, padding: _size_2_t = 0, dilation: _size_2_t = 1,
+                 bias: bool = False, eta: float = 1e-5, downsample_perc=1.0,
+                 device=None, dtype=None) -> None:
 
         factory_kwargs = {'device': device, 'dtype': dtype}
 
         # define decorrelation layer
-        super().__init__(in_features=in_channels * np.prod(kernel_size), kappa=kappa, **factory_kwargs)        
+        super().__init__(in_features=in_channels * np.prod(kernel_size), eta=eta, **factory_kwargs)        
         self.downsample_perc = downsample_perc
 
         self.in_channels = in_channels
@@ -171,6 +180,6 @@ class DecorConv2d(Decorrelation):
                                     dilation=self.dilation).moveaxis(1, 3).reshape(-1, self.in_features)
         
     def update(self):
-        self.X = self.decorrelate(self.input[np.random.choice(np.arange(len(self.input)),
+        self.decor_state = self.decorrelate(self.input[np.random.choice(np.arange(len(self.input)),
                                                       size= int(len(self.input) * self.downsample_perc))])
         return super().update()
