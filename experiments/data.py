a986b6ec09400a6329c5a38b98adf0f699659956
Kieran Carrigg <kieran.carrigg@donders.ru.nl>
2024-05-17 17:25:26 +0200

    Update

--- experiments/data.py
+++ experiments/data.py
@@ -56,7 +56,7 @@ def get_CIFAR10(args):
     if args.test_samples == 0:
         test_loader = None
     else:
-        test_data = CIFAR10(root=args.data_path, train=True, download=True, transform=transform)
+        test_data = CIFAR10(root=args.data_path, train=False, download=True, transform=transform)
 
         if args.test_samples != -1:
             test_data = Subset(test_data, np.random.permutation(len(test_data.data))[:args.test_samples])
